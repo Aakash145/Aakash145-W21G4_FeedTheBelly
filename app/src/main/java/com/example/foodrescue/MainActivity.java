8ca7597bdf79350c872466f151792edc503a4519
Aakash Saini <aakashsaini769@gmail.com>
2021-02-24 23:30:14 -0800

    Adding Splash Screen

--- app/src/main/java/com/example/foodrescue/MainActivity.java
+++ app/src/main/java/com/example/foodrescue/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.foodrescue;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -16,6 +17,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar myBar = getSupportActionBar();
+        myBar.setDisplayHomeAsUpEnabled(true);
+        myBar.setDisplayUseLogoEnabled(true);
+        myBar.setLogo(R.mipmap.ic_launcher_round);
 
         registerButton = findViewById(R.id.registerButton);
 
