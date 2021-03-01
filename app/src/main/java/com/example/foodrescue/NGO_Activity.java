ff693c2fa32a791c1b2af5f8e6b36270669c2dd2
dhansukhlal-m8615 <shivananda@student.douglascollege.ca>
2021-02-28 19:08:22 -0600

    Added NGO_Activity activity

---
+++ app/src/main/java/com/example/foodrescue/NGO_Activity.java
@@ -0,0 +1,14 @@
+package com.example.foodrescue;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NGO_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_n_g_o_);
+    }
+}
