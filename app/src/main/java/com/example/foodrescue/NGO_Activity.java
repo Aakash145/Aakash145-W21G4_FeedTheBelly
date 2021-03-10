25ad05dd798b65add13850786b863dff8b0f06f5
Minal Dhansukhlal <dhansukhlalm@student.douglascollege.ca>
2021-03-09 21:32:56 -0600

    Added NGO Dashboard

--- app/src/main/java/com/example/foodrescue/NGO_Activity.java
+++ app/src/main/java/com/example/foodrescue/NGO_Activity.java
@@ -3,10 +3,13 @@ package com.example.foodrescue;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class NGO_Activity extends AppCompatActivity {
-
+ Button submit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,5 +17,12 @@ public class NGO_Activity extends AppCompatActivity {
 
         ActionBar actionBar=getSupportActionBar();
         actionBar.setTitle("Not-For-Profit Registration");
+
+        submit=findViewById(R.id.buttonSubmit);
+        submit.setOnClickListener((View view)->{
+            Intent myIntent = new Intent(NGO_Activity.this, NGO_Dashboard.class);
+            startActivity(myIntent);
+        });
+
     }
 }
