3618d386abae2010ca55c12ea87886593f2497b7
Minal Dhansukhlal <dhansukhlalm@student.douglascollege.ca>
2021-03-30 14:04:53 -0500

    Update unversioned files

---
+++ app/src/main/java/com/example/foodrescue/Restaurant_List_Adapter.java
@@ -0,0 +1,86 @@
+package com.example.foodrescue;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class  Restaurant_List_Adapter extends RecyclerView.Adapter<Restaurant_List_Adapter.ViewHolder> {
+    Context context;
+    ArrayList restEmails,restNames,restPhones,restIDs,restAddress,restPostal;
+    RecyclerView rvDonations;
+    public Restaurant_List_Adapter(Context context, ArrayList restEmails, ArrayList restNames, ArrayList restPhones, ArrayList restIDs, ArrayList restAddress, ArrayList restPostal) {
+        this.context = context;
+        this.restEmails = restEmails;
+        this.restNames = restNames;
+        this.restPhones = restPhones;
+        this.restIDs = restIDs;
+        this.restAddress = restAddress;
+        this.restPostal = restPostal;
+    }
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        TextView rowId;
+        TextView rowName;
+        TextView rowEmail;
+        TextView rowAddress;
+        TextView rowPhone;
+        TextView rowPostal;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            rowId=itemView.findViewById(R.id.rest_ID);
+            rowName=itemView.findViewById(R.id.rest_name);
+            rowEmail=itemView.findViewById(R.id.rest_Email);
+            rowAddress=itemView.findViewById(R.id.rest_Address);
+            rowPhone=itemView.findViewById(R.id.rest_Phone);
+            rowPostal=itemView.findViewById(R.id.rest_Postal);
+        }
+
+    }
+
+
+    @NonNull
+    @Override
+    public Restaurant_List_Adapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view=LayoutInflater.from(parent.getContext()).inflate(R.layout.single_restaurant,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Restaurant_List_Adapter.ViewHolder holder, int position) {
+        holder.rowId.setText("Restaurant ID: "+String.valueOf(restIDs.get(position)));
+        holder.rowName.setText("Name: "+String.valueOf(restNames.get(position)));
+        holder.rowAddress.setText("Address: "+String.valueOf(restAddress.get(position)));
+        holder.rowEmail.setText("Email: "+String.valueOf(restEmails.get(position)));
+        holder.rowPhone.setText("Phone: "+String.valueOf(restPostal.get(position)));
+        holder.rowPostal.setText("Postal Code: "+String.valueOf(restPhones.get(position)));
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(context,recyclerView_NGO.class);
+                intent.putExtra("EMAIL",restEmails.get(position).toString());
+                intent.putExtra("ADDRESS",String.valueOf(restAddress.get(position)));
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return restIDs.size();
+    }
+
+
+}
---
+++ app/src/main/java/com/example/foodrescue/Restaurants_List_Recycler.java
@@ -0,0 +1,70 @@
+package com.example.foodrescue;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import java.util.ArrayList;
+
+public class Restaurants_List_Recycler extends AppCompatActivity {
+    SQLiteDatabase FeedTheBelly;
+    RecyclerView rvRestaurants;
+    ArrayList<String> ids;
+    ArrayList<String> names;
+    ArrayList<String> address;
+    ArrayList<String> city;
+    ArrayList<String> state;
+    ArrayList<String> country;
+    ArrayList<String> postal;
+    ArrayList<String> emails;
+    ArrayList<String> phones;
+    DatabaseHelper myDb;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurants__list__recycler);
+        myDb = new DatabaseHelper(this);
+        ActionBar myActionBar=getSupportActionBar();
+        myActionBar.setTitle("Restaurants List");
+
+        rvRestaurants=findViewById(R.id.rvRestaurantsList);
+        rvRestaurants.setLayoutManager(new LinearLayoutManager(this));
+        ids=new ArrayList<>();
+        names=new ArrayList<>();
+        emails=new ArrayList<>();
+        address=new ArrayList<>();
+        city=new ArrayList<>();
+        state=new ArrayList<>();
+        country=new ArrayList<>();
+        postal=new ArrayList<>();
+        phones=new ArrayList<>();
+        Cursor cursor=myDb.readAllRestaurants();
+        while(cursor.moveToNext()){
+
+            try {
+                emails.add(cursor.getString(0));
+                names.add(cursor.getString(1));
+                phones.add(cursor.getString(2));
+                ids.add(cursor.getString(3));
+                postal.add(cursor.getString(8));
+                String completeAddress=(cursor.getString(4))+", "+(cursor.getString(5))
+                        +", "+(cursor.getString(6))+", "+(cursor.getString(7));
+                address.add(completeAddress);
+                }catch(Exception e){
+                Log.d("Error",e.getMessage());
+            }
+        }
+        Restaurant_List_Adapter myAdapter = new Restaurant_List_Adapter(Restaurants_List_Recycler.this, emails, names, phones,
+                ids, address,postal);
+        rvRestaurants.setAdapter(myAdapter);
+
+    }
+}
