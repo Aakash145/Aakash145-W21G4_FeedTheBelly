1693173a73d845bdf1407a4311143625ad565925
Minal Dhansukhlal <dhansukhlalm@student.douglascollege.ca>
2021-03-31 15:25:55 -0500

    Solved splash and fragments issue

--- app/src/main/java/com/example/foodrescue/SplashActivity.java
+++ app/src/main/java/com/example/foodrescue/SplashActivity.java
@@ -8,13 +8,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Timer;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+
+       // Timer timer=new Timer();
+       // TimerTask task=new TimerTask() {
+          //  @Override
+          //  public void run() {
+                Intent intent=new Intent(SplashActivity.this, Intro_Pages.class);
+                startActivity(intent);
+       //     }
+      //  };
+        //timer.schedule(task,10000);
 
     }
 }
